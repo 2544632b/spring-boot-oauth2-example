93f3114ee8e99dee3f6a9eaa77bd1252c77ebb7f
little fairy <2578997513@qq.com>
2023-09-11 10:35:01 +0800

    Change imread

--- frontend-module/auto_primary_color.py
+++ frontend-module/auto_primary_color.py
@@ -1,4 +1,6 @@
 import cv2
+import urllib
+import urllib.parse
 import urllib.request
 import matplotlib.pyplot as plt
 import numpy as np
@@ -27,50 +29,51 @@ def plot_colors(hist, centroids):
 
     return bar
 
-clusters = 5
-iterations = 10
 
-imageFromURL = urllib.request.urlopen("23333")
+def get_color(path):    # Publicly for javascript
+    response = urllib.request.urlopen(path)
 
-image = np.asarray(bytearray(imageFromURL.end()), dtype="uint8")
+    image = np.asarray(bytearray(response.read()), dtype="uint8")
+    image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+    # image = cv2.imread(image, 17)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-image = cv2.imread("IMG_1184(1).JPG", 17)
-image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image = image.reshape((image.shape[0] * image.shape[1], 3))
 
-image = image.reshape((image.shape[0] * image.shape[1], 3))
+    clt = KMeans(5)
+    clt.fit(image)
 
-clt = KMeans(clusters)
-clt.fit(image)
+    hist = centroid_histogram(clt)
+    bar = plot_colors(hist, clt.cluster_centers_)
 
-hist = centroid_histogram(clt)
-bar = plot_colors(hist, clt.cluster_centers_)
+    pixel = [0, 0, 0]
+    targetPixel = [0, 0, 0]
 
-pixel = [0, 0, 0]
-targetPixel = [0, 0, 0]
+    for i in bar:   # ????????
+        for j in i:     # Best match in historgram (1*m)
+            if pixel[0] == 0 and pixel[1] == 0 and pixel[2] == 0:
+                pixel[0] = j[0]
+                pixel[1] = j[1]
+                pixel[2] = j[2]
 
-for i in bar:   # ????????
-    for j in i:     # Best match in historgram (1*m)
-        if pixel[0] == 0 and pixel[1] == 0 and pixel[2] == 0:
-            pixel[0] = j[0]
-            pixel[1] = j[1]
-            pixel[2] = j[2]
-
-        if pixel[0] == j[0] and pixel[1] == j[1] and pixel[2] == j[2]:
-            continue
-        
-        if pixel[0] != j[0] or pixel[1] != j[1] or pixel[2] != j[2]:
-            brightness = 0.3 * pixel[0] + 0.6 * pixel[1] + 0.1 * pixel[2]
-            targetBrightness = 0.3 * j[0] + 0.6 * j[1] + 0.1 * j[2]
-            if targetBrightness >= brightness:
+            if pixel[0] == j[0] and pixel[1] == j[1] and pixel[2] == j[2]:
                 continue
-            else:
-                targetPixel = j    # Given a pixel
+            
+            if pixel[0] != j[0] or pixel[1] != j[1] or pixel[2] != j[2]:
+                brightness = 0.3 * pixel[0] + 0.6 * pixel[1] + 0.1 * pixel[2]
+                targetBrightness = 0.3 * j[0] + 0.6 * j[1] + 0.1 * j[2]
+                if targetBrightness >= brightness:
+                    continue
+                else:
+                    targetPixel = j    # Given a pixel
 
-            break
+                break
 
-print(targetPixel)
+    return targetPixel
 
-plt.figure()
-plt.axis("off")
-plt.imshow(bar)
-plt.show()
+    # plt.figure()
+    # plt.axis("off")
+    # plt.imshow(bar)
+    # plt.show()
+
+print(get_color("https://www.wenjiangs.com/wp-content/uploads/2020/03/scikit-learn-logo.jpg"))
